39cdecc19ddae73987af23470ecf1c86820c78df
nisarg <shahnisarg23@gmail.com>
2025-02-09 16:42:38 +0530

    netlify changes

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -6,16 +6,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      },
-    },
+    outDir: 'dist',
   },
 });
